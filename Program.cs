a4b13847be3b92761f9b689d7321a37f9c0a684f
Noorudinz <noorudeens@yahoo.in>
2023-05-24 19:34:28 +0530

    error corrected

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to CICD Testing console app from .net core 7.0 using jenkins");
-//This is have an error 
-Console.WriteLine("Missing comma")
+//No error
+Console.WriteLine("Azure pipeline");
 
