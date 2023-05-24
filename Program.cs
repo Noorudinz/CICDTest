8d5761dff03a887719bf46719d64f9146c395d8c
Noorudinz <noorudeens@yahoo.in>
2023-05-24 15:32:35 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to CICD Testing console app from .net core 7.0 using jenkins");
+
